9ec3ebe59ddba57493aae05b5d3cf405c0e641f5
NathanDuda <nathanduda02@gmail.com>
2024-11-17 00:46:59 -0500

    building NCBI connection

--- app/Scripts/NCBI_connection.R
+++ app/Scripts/NCBI_connection.R
@@ -1,123 +1,216 @@
 
+library(Biostrings)
+library(biomartr)
+
+
+# the possible inputs 
+available_databases <- c('refseq', 'genbank', 'ensembl') # for prot and cds
+available_organisms <- listGenomes()
+data_types <- c('Proteomes', 'CDS', 'Genomes')
+keep_which_transcript <- c('longest', 'first')
+
 
 
 ###
 
-
-
-
-available_databases <- c('refseq', 'genbank', 'ensembl')
-selected_database <- 'ensembl'
-
-selected_organisms <- c('Drosophila mojavensis', 'Drosophila melanogaster', 'Drosophila ananassae')
-
-
-get_data_for_organisms <- function(selected_organisms, data_types, selected_database) {
+get_data_for_organisms <- function(selected_organisms, data_types, selected_database_protein, selected_database_cds, selected_database_genome, prot_data_dir, cds_data_dir, genome_data_dir) {
   
-  if ('set_proteomes' %in% data_types){
-    output_directory <- paste0(here_duplica, '/set_proteomes')
-    getProteomeSet(db = selected_database, organism = selected_organisms, path = output_directory)
+  if ('Proteomes' %in% data_types){
+    getProteomeSet(db = selected_database_protein, organism = selected_organisms, path = prot_data_dir)
   }
 
-  if ('set_CDS' %in% data_types){
-    output_directory <- paste0(here_duplica, '/set_CDS')
-    getCDSSet(db = selected_database, organism = selected_organisms, path = output_directory)
+  if ('CDS' %in% data_types){
+    getCDSSet(db = selected_database_cds, organism = selected_organisms, path = cds_data_dir)
   }
   
-  if ('set_genomes' %in% data_types){
-    output_directory <- paste0(here_duplica, '/set_genomes')
-    getGenomeSet(db = selected_database, organism = selected_organisms, path = output_directory)
+  if ('Genomes' %in% data_types){
+    getGenomeSet(db = selected_database_genome, organism = selected_organisms, path = genome_data_dir)
   }
   
   
 }
 
-get_data_for_organisms(selected_organisms, c('set_proteomes', 'set_CDS', 'set_genomes'), selected_database)
-
-
-#
-
-library(Biostrings)
-
-
-files <- list.files('C:/Users/17735/Downloads/DuplicA/set_proteomes/', full.names = TRUE)
-
-i <- 1
-
-selected_organisms <- gsub(' ', '_', selected_organisms)
-
-aa_file <- files[grepl(selected_organisms[i], files) & grepl("\\.pep\\.", files)]
-fasta_data <- readAAStringSet(aa_file)
-
-
-t <- as.data.frame(fasta_data) %>%
-  rownames_to_column('names') %>%
-  mutate(
-    protein_id = str_extract(names, "^[^ ]+"),
-    gene_id = str_extract(names, "(?<=gene:)[^ ]+"),
-    transcript_id = str_extract(names, "(?<=transcript:)[^ ]+")
-  ) %>%
-  select(gene_id, transcript_id, protein_id, prot_seq = x) %>%
-  mutate(len = nchar(prot_seq)) 
-
-
-any(duplicated(t$transcript_id)) # should be FALSE
-any(duplicated(t$protein_id)) # should be FALSE
-
-if (keep_which == 'longest'){
-  x <- t %>%
-    group_by(gene_id) %>%
-    filter(len == max(len)) %>% 
-    slice_head(n = 1) %>%  # keep only one even if max length is the same 
-    ungroup() 
+get_organisms_prot_fasta_data <- function(selected_organism, prot_files) {
+  prot_file <- prot_files[grepl(selected_organism, prot_files) & grepl("\\.pep\\.", prot_files)]
+  prot_fasta_data <- readAAStringSet(prot_file)
+  
+  return(prot_fasta_data)
+}
+get_organisms_cds_fasta_data <- function(selected_organism, cds_files) {
+  cds_file <- cds_files[grepl(selected_organism, cds_files) & grepl("\\.cds\\.", cds_files)]
+  cds_fasta_data <- readDNAStringSet(cds_file)
+  
+  return(cds_fasta_data)
+  
 }
 
-if (keep_which == 'first'){
-  x <- t %>%
-    group_by(gene_id) %>%
-    slice_head(n = 1) %>%  
-    ungroup() 
+get_prot_transcript_seq <- function(prot_fasta_data, keep_which_transcript) {
+  prot_seqs_df <- as.data.frame(prot_fasta_data) %>%
+    rownames_to_column('names') %>%
+    mutate(
+      protein_id = str_extract(names, "^[^ ]+"),
+      gene_id = str_extract(names, "(?<=gene:)[^ ]+"),
+      transcript_id = str_extract(names, "(?<=transcript:)[^ ]+")
+    ) %>%
+    select(gene_id, transcript_id, protein_id, prot_seq = x) %>%
+    mutate(len = nchar(prot_seq)) 
+  
+  
+  any(duplicated(prot_seqs_df$transcript_id)) # should be FALSE
+  any(duplicated(prot_seqs_df$protein_id)) # should be FALSE
+  
+  if (keep_which_transcript == 'longest'){
+    prot_seqs_df <- prot_seqs_df %>%
+      group_by(gene_id) %>%
+      filter(len == max(len)) %>% 
+      slice_head(n = 1) %>%  # keep only one even if max length is the same 
+      ungroup() 
+  }
+  
+  if (keep_which_transcript == 'first'){
+    prot_seqs_df <- prot_seqs_df %>%
+      group_by(gene_id) %>%
+      slice_head(n = 1) %>%  
+      ungroup() 
+  }
+  any(duplicated(prot_seqs_df$gene_id)) # should be FALSE
+  
+  return(prot_seqs_df)
 }
-any(duplicated(x$gene_id)) # should be FALSE
+add_cds_transcript_seq <- function(cds_fasta_data, prot_seqs_df) {
+  
+  cds_seqs_df <- as.data.frame(cds_fasta_data) %>%
+    rownames_to_column('names') %>%
+    mutate(transcript_id = str_extract(names, "^[^ ]+")) %>%
+    select(transcript_id, cds_seq = x)
+  
+  seqs_df <- merge(cds_seqs_df, prot_seqs_df, by = 'transcript_id')
+  
+  seqs_df <- seqs_df %>% select(gene_id, transcript_id, protein_id, cds_seq, prot_seq)
+  
+  return(seqs_df)
+}
+
+create_output_dirs <- function() {
+  prot_output_dir <- paste0(here_results, '/Fastas/Protein_Fastas/')
+  dir.create(prot_output_dir, recursive = T)
+  cds_output_dir <- paste0(here_results, '/Fastas/Nucelotide_Fastas/')
+  dir.create(cds_output_dir, recursive = T)
+  
+  return(list(prot_output_dir = prot_output_dir, cds_output_dir = cds_output_dir))
+}
+seq_df_to_fasta_files <- function(seqs_df, prot_output_dir, cds_output_dir, selected_organism) {
+  
+  prot_output_file <- paste0(prot_output_dir, selected_organism, '_prot.fasta')
+  cds_output_file <- paste0(cds_output_dir, selected_organism, '_cds.fasta')
+  
+  seqs_df %>%
+    mutate(fasta = paste0('>', gene_id, '\n', prot_seq)) %>%
+    pull(fasta) %>%
+    writeLines(con = prot_output_file)
+  
+  seqs_df %>%
+    mutate(fasta = paste0('>', gene_id, '\n', cds_seq)) %>%
+    pull(fasta) %>%
+    writeLines(con = cds_output_file)
+}
+
+move_genome_files <- function(selected_organism, genome_files) {
+  
+  
+  # get the genome of the given species 
+  genome_file <- genome_files[grepl(selected_organism, genome_files) & grepl("\\.dna\\.", genome_files)]
+  
+  if (length(genome_file) == 0) {
+    print(paste0('No genome file found for ', selected_organism))
+    return()
+  }
+  
+  # create a new directory for the genome files
+  genome_output_dir <- paste0(here_results, '/Fastas/Genome_Fastas/')
+  dir.create(genome_output_dir, recursive = T)
+  
+  # copy the file to the output directory
+  file.copy(genome_file, new_genome_file)
+  
+  # rename the new file 
+  new_genome_file <- paste0(genome_output_dir, selected_organism, '_genome.fasta')
+  
+  
+  print(genome_file)
+  print(length(genome_file))
+  print(new_genome_file)
+  print(length(new_genome_file))
+
+  file.rename(genome_file, new_genome_file)
+  file.remove(paste0(genome_output_dir, '/', basename(genome_file)))
+  
+  
+  
+}
+
+main_ncbi <- function(selected_organisms, data_types, selected_database_protein, selected_database_cds, selected_database_genome, keep_which_transcript) {
+  
+  selected_organisms <- gsub(' ', '_', selected_organisms)
+  
+  prot_data_dir <- paste0(here_results, '/ncbi_output/protein_data')
+  cds_data_dir <- paste0(here_results, '/ncbi_output/cds_data')
+  genome_data_dir <- paste0(here_results, '/ncbi_output/genome_data')
+  
+  # get the data types from ncbi 
+  get_data_for_organisms(selected_organisms, data_types, selected_database_protein, selected_database_cds, selected_database_genome, prot_data_dir, cds_data_dir, genome_data_dir)
+  
+  # list all files provided by ncbi 
+  prot_files <- list.files(prot_data_dir, full.names = TRUE)
+  cds_files <- list.files(cds_data_dir, full.names = TRUE)
+  genome_files <- list.files(genome_data_dir, full.names = TRUE)
+
+  # interate over each selected organism 
+  for (selected_organism in selected_organisms) {
+    
+    # read in the fasta files for the given organism 
+    prot_fasta_data <- get_organisms_prot_fasta_data(selected_organism, prot_files)
+    cds_fasta_data <- get_organisms_cds_fasta_data(selected_organism, cds_files)
+    
+    # format the fasta data into a table. keep only one sequence per gene
+    prot_seqs_df <- get_prot_transcript_seq(prot_fasta_data, keep_which_transcript = 'longest')
+    seqs_df <- add_cds_transcript_seq(cds_fasta_data, prot_seqs_df)
+    
+    # write the sequences into fasta files 
+    dirs <- create_output_dirs()
+    seq_df_to_fasta_files(seqs_df, dirs$prot_output_dir, dirs$cds_output_dir, selected_organism)
+    
+    
+    # write genome fastas if selected
+    if ('Genomes' %in% data_types) {
+      move_genome_files(selected_organism, genome_files)
+    }
+    
+  }
+  
+  # delete the ncbi output directory
+  unlink(paste0(here_results, '/ncbi_output'), recursive = T)
+  
+}
 
 
 
 
 
-files <- list.files('C:/Users/17735/Downloads/DuplicA/set_CDS/', full.names = TRUE)
 
-cds_file <- files[grepl(selected_organisms[i], files) & grepl("\\.cds\\.", files)]
-fasta_data <- readDNAStringSet(cds_file)
-
-
-t <- as.data.frame(fasta_data) %>%
-  rownames_to_column('names') %>%
-  mutate(
-    transcript_id = str_extract(names, "^[^ ]+")
-  ) %>%
-  select(transcript_id, cds_seq = x)
+prot_output_dir <- paste0(here_results, '/Fastas/Protein_Fastas/')
 
 
 
-b <- merge(t, x, by = 'transcript_id')
+# example input
+selected_database_protein <- 'refseq'
+selected_database_cds <- 'refseq'
+selected_database_genome <- 'ensembl'
 
+selected_organisms <- c('Deinococcus radiodurans', 'Piscirickettsia salmonis', 'Drosophila ananassae')
+data_types <- c('Proteomes', 'CDS', 'Genomes')
+keep_which_transcript <- 'longest'
 
-
-# write to a fasta file 
-
-
-
-
-
-
-
-
-
-
-
-genome_file <- files[grepl(selected_organisms[i], files) & grepl("\\.dna\\.", files)]
-fasta_data <- readDNAStringSet(genome_file)
-
-
+main_ncbi(selected_organisms, data_types, selected_database_protein, selected_database_cds, selected_database_genome, keep_which_transcript)
 
 
