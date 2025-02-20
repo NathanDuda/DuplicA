d201f72ea3de0614d0d7036ba65e7dca3aa2358c
ALI-VALI-007 <alisvali007@gmail.com>
2025-02-19 23:21:09 -0500

    Fixed the router

--- app/Front_end/vite-project/src/App.jsx
+++ app/Front_end/vite-project/src/App.jsx
@@ -3,22 +3,35 @@ import Home from "./Home";
 import Workflow from "./Workflow";
 import Visualization from "./Visualization";
 import "./App.css";
+import { BrowserRouter, Route,Routes,Link } from 'react-router-dom';
+import HypothesisTesting from './HypothesisTesting';
+
+import About from './About.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <h1 className="logo">DuplicA</h1>
-        <nav className="nav">
-          <a href="#home" className="nav-link">Home</a>
-          <a href="#workflow" className="nav-link">WorkFlow</a>
-          <a href="#visualization" className="nav-link">Visualization</a>
-          <a href="#hypothesis" className="nav-link">Hypothesis Testing</a>
-          <a href="#about" className="nav-link">About</a>
-        </nav>
-      </header>
-      <Home />
-    </div>
+    //<div className="workflow-container">
+    <BrowserRouter>
+      <div>
+        <header className="header">
+          <h1 className="logo">DuplicA</h1>
+          <nav className="nav">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/Workflow" className="nav-link">Work Flow</Link>
+            <Link to="/Visualization" className="nav-link">Visualization</Link>
+            <Link to="/hypothesis" className="nav-link">Hypothesis Testing</Link>
+            <Link to="/about" className="nav-link">About</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Workflow" element={<Workflow />} />
+          <Route path="/Visualization" element={<Visualization />} />
+          <Route path="/hypothesis" element={<HypothesisTesting />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
