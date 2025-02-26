83a1699801d4be2f07edea1ef493fa0a04686f8c
ALI-VALI-007 <alisvali007@gmail.com>
2025-02-26 16:43:25 -0500

    Made relative

--- app/run_api.R
+++ app/run_api.R
@@ -1,9 +1,7 @@
 library(plumber)
 
+curDir <- getwd()
+curDir <- dirname(curDir)
 
-prefix <- '/mnt/c'
-prefix <- 'C:'
-
-r <- plumb(paste0(prefix, "/Users/17735/Downloads/DuplicA/app/api.R"))
+r <- plumb(paste0(curDir, "/app/api.R"))
 r$run(host = "0.0.0.0", port = 8000)
-
